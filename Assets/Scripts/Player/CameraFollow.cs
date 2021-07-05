0d4cb30ad25544c59b62b59c4c23c0ccdce88aab
longking0803 <50902413+longking0803@users.noreply.github.com>
2021-07-05 20:02:54 +0700

    Change UI for the menu
    + Add new background
    - Edit button layout

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -4,8 +4,8 @@ public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform target;
-    [SerializeField] private Vector3 offset;
-    [SerializeField] [Range(0.01f, 1f)]private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset = new Vector3(0.0f,2.35f,-6.6f);
+    [SerializeField] [Range(0.01f, 1f)]private float smoothSpeed = 0.2f;
     private Vector3 velocity = Vector3.zero;
 
     void LateUpdate()
