17d0aee4d57b0fda13f901129705aadbf561e228
saya9729 <ductaiprono1@gmail.com>
2021-07-16 10:33:59 +0700

    Add save game script
    + Add DataManager.cs, PlayerData.cs
    - Modify MainMenu.cs to add save game function

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    [SerializeField] private DataManager dataManager;
+    public void ContinueGame()
+    {
+        dataManager.Load();
+        player.position = dataManager.data.position;
+        SceneManager.LoadScene(dataManager.data.currentLevel);
+    }
+    public void SaveGame()
+    {
+        dataManager.data.position = new Vector2(player.position.x, player.position.y);
+        dataManager.data.currentLevel = SceneManager.GetActiveScene().name;
+        dataManager.Save();
+    }
     public void ExitButton()
     {
         Debug.Log("Quit");
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerData 
+{
+    public Vector2 position= new Vector2(0,0);
+
+    public string currentLevel = "";
+}
