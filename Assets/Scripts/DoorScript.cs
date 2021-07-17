b7b1aa4f037b9301bccb282df920e31581909dca
saya9729 <ductaiprono1@gmail.com>
2021-07-17 23:01:05 +0700

    add image

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorScript : MonoBehaviour
 {
     [SerializeField] private DataManager dataManager;
+    [SerializeField] private GameObject image;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            dataManager.NextLevel();
+            if (SceneManager.GetActiveScene().name == "The Level 2")
+            {
+                image.SetActive(true);
+                //StartCoroutine(wait(20));
+            }
+            else
+            {
+                dataManager.NextLevel();
+            }
         }
+    }
+
+    IEnumerator wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        //dataManager.NextLevel();
     }
 }
